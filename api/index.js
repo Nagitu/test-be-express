0f5d6067c13c8797846860d683cbba4629cf9654
Nagitu <denipraa1@gmail.com>
2025-05-19 17:58:13 +0700

    test get profile

--- api/index.js
+++ api/index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
 });
 
+app.get("/get-profile", (_, res) => {
+  res.json({
+    message: "test baru",
+    data: {
+      email: "email",
+      password: "password",
+    },
+  });
+});
+
 app.get("/about", (req, res) => {
   res.send("This is the About page.");
 });
